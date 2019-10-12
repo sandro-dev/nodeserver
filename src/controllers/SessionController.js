e15abebaee63391a1463c767c2e8efe8f3ab125a
Sandro Santos <sandro.dev@gmail.com>
2019-10-12 13:40:49 -0300

    SessionController with methods Store and Index: Insert user and list users

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,7 +1,21 @@
+const User = require('../models/User');
 //index, show, store, update, destroy
 
 module.exports = {
-  store(req, res) {
-    return res.json({message: 'Session was registred...'});
+  async store(req, res) {
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if(!user){
+      user = await User.create({ email });
+    }    
+    
+    return res.json(user);
+  },
+  
+  async index(req, res) {
+    const users = await User.find();
+    return res.json(users);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,37 +3,7 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', SessionController.store);
-
-routes.get('/users', (req, res)=>{
-  return res.json({
-    "users": 
-    [
-      {
-        "id": "62515963",
-        "name": "Elmore Altenwerth",
-        "phone": "(433) 203-9327",
-        "address": {
-          "street": "2447 Colten Trail",
-          "city": "Jamarside",
-          "state": "Arizona"
-          }
-      },
-
-      {
-        "id": "77437088",
-          "name": "Bert Flatley",
-          "phone": "(071) 403-5391",
-          "address": {
-            "street": "47688 Trantow Mills",
-            "city": "Weberfort",
-            "state": "Virginia"
-          }
-      }
-    ]
-
-  }
-  );
-});
+routes.post('/sessions', SessionController.store);
+routes.get('/sessions', SessionController.index);
 
 module.exports = routes;
