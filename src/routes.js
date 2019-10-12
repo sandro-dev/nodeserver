3456ade3cb3f60197bb9ac75287d922a92bb0291
Sandro Santos <sandro.dev@gmail.com>
2019-10-12 13:15:05 -0300

    pattern MVC: was added User Model and SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', (req, res)=>{
-  return res.json(req.body);
-});
+routes.post('/users', SessionController.store);
 
 routes.get('/users', (req, res)=>{
   return res.json({
