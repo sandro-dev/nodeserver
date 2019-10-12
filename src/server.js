983828757afb2a3ea5f464ca05eb15ccb1b96d61
Sandro Santos <sandro.dev@gmail.com>
2019-10-12 12:54:51 -0300

    mongoose connect to mondoDB Atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
+
 const routes = require('./routes');
 
 const app = express();
+mongoose.connect('mongodb+srv://omnistack:omnistack9@cluster0-hk4sr.mongodb.net/semana09?retryWrites=true&w=majority',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
